a8b3c59c7e771453f266419e703380432cbc419f
sachin <sachin.g@itpreneurpune.com>
2025-12-11 13:20:29 +0530

    task added in index files of prevetdefault and importExport folder

---
+++ JavaScript/ImportExport/script1.js
@@ -0,0 +1,12 @@
+let a="Hello";
+let b="Welcome";
+
+let obj={
+    id:1,
+    name:"aman"
+}
+
+
+
+// export default {a,b};  //single data
+export  {a,b,obj};
---
+++ JavaScript/ImportExport/script2.js
@@ -0,0 +1,11 @@
+
+// import {a,b} from './script1.js';
+// console.log(a +" "+b);
+
+import {a as c,b as d , obj} from './script1.js';
+console.log(c +" "+d);
+console.log(obj)
+console.log(obj)
+console.log(obj)
+console.log(obj)
+
